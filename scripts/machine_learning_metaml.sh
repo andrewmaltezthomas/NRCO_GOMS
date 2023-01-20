d8963ce81d694b2a490066eabfbd79e00c2030db
andrewmaltezthomas <andrewmaltezthomas@hotmail.com>
2023-01-20 21:12:49 +0000

    cleaned up files

--- scripts/machine_learning_metaml.sh
+++ scripts/machine_learning_metaml.sh
@@ -2,7 +2,7 @@
 git pull https://github.com/SegataLab/metaml
 
 # Run LODO
-declare -a datasets=("DerosaL_2020" "DerosaL_2022" "PRIMM_NL_LeeK_2022" "PRIMM_UK_LeeK_2022" "Manchester_LeeK_2022" "FrankelAE_2017" "GopalakrishnanV_2018" "McCullochJA_2022" "RoutyB_2022_Renal" "RoutyB_2022_Lung")
+declare -a datasets=("DerosaL_2020" "DerosaL_2022" "PRIMM_NL_LeeK_2022" "PRIMM_UK_LeeK_2022" "Manchester_LeeK_2022" "FrankelAE_2017" "GopalakrishnanV_2018" "McCullochJA_2022" "RoutyB_2018_Lung")
 for i in "${datasets[@]}"
 do
 python classification_thomas-manghi.py MetaML_ORR_PDvsSDPRCR.txt ${i}_ORR_PDvsSDPRCR_rf -d 1:ORR:R -t Cohort:${i} -nt 1000 -nc 10 -nsl 5 -mf auto -hv 1 -df -r 1 -p 1 -z t__
